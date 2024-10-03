56259398706afa667bf6d8aec9570b7c1b6af96c
Jamie Cornick <jamie.cornick@keyin.com>
2024-10-03 09:42:28 -0230

    Added example of mocking an object.

--- src/main/java/com/keyin/Example.java
+++ src/main/java/com/keyin/Example.java
@@ -1,9 +1,11 @@
 package com.keyin;
 
+import javax.xml.crypto.Data;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Example {
+    private DataSource dataSource;
 
     public Date getTodaysDate() {
         return new Date();
@@ -11,5 +13,21 @@ public class Example {
 
     public int getCurrentMonth() {
         return Calendar.getInstance().get(Calendar.MONTH);
+    }
+
+    public String getDataSourceName() {
+        return getDataSource().getDataSourceName();
+    }
+
+    public DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new DataSource();
+        }
+
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 }
--- src/test/java/com/keyin/ExampleTest.java
+++ src/test/java/com/keyin/ExampleTest.java
@@ -2,11 +2,19 @@ package com.keyin;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Calendar;
 import java.util.Date;
 
+@ExtendWith(MockitoExtension.class)
 public class ExampleTest {
+    @Mock
+    private DataSource dataSourceMock;
+
 
     @Test
     public void testGetTodaysDate() {
@@ -20,6 +28,17 @@ public class ExampleTest {
         Example exampleUnderTest = new Example();
 
         Assertions.assertEquals(Calendar.getInstance().get(Calendar.MONTH), exampleUnderTest.getCurrentMonth());
+    }
+
+    @Test
+    public void testDataSourceName() {
+        Example exampleUnderTest = new Example();
+
+        Mockito.when(dataSourceMock.getDataSourceName()).thenReturn("TestDataSource");
+
+        exampleUnderTest.setDataSource(dataSourceMock);
+
+        Assertions.assertEquals("TestDataSource", exampleUnderTest.getDataSourceName());
     }
 
 }
